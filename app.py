0515d005a6933c4584661231fca8dee4a4b2ef4f
Juan Manuel Casal <juan.manuel.casal@istea.com.ar>
2025-03-28 13:23:56 -0300

    cambiando puerto app.py

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def resultados():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto asignado por Vercel
+    serve(app, host="0.0.0.0", port=port)
